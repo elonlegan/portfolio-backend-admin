d1c3fdf6999d143741fa155a4af9ceff6b8b966f
Angel Leon <angel@leanware.io>
2023-04-25 22:49:50 -0500

    fixed prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'portfolio-backend-angel.up.railway.app',
+  apiUrl: 'https://portfolio-backend-angel.up.railway.app/projects',
   CLOUDINARY_URL: 'https://api.cloudinary.com/v1_1/dev-empty/image/upload',
 };
