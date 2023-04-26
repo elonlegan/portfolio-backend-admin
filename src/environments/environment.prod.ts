daa6d004803402389ea9db3f06612b1c05fe05d0
Angel Leon <angel@leanware.io>
2023-04-26 12:41:27 -0500

    fix apiURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://portfolio-backend-angel.up.railway.app/projects',
+  apiUrl: 'https://portfolio-backend-angel.up.railway.app',
   CLOUDINARY_URL: 'https://api.cloudinary.com/v1_1/dev-empty/image/upload',
 };
